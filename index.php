dc994483770df3d4531de3d5ff52ed618c9895a9
anbvjkannv12 <144758408+anbvjkannv12@users.noreply.github.com>
2025-05-25 00:53:57 +0800

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include 'includes/header.php'; ?>
-<main>
-  <h1>遠離毒品，珍惜生命</h1>
-  <p>本網站致力於推廣毒品防制知識，讓更多人了解毒品的危害與預防方式。</p>
-  <img src="assets/images/stop-drugs.jpg" alt="拒絕毒品">
-</main>
-<?php include 'includes/footer.php'; ?>
